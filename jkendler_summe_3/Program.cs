e19ed76ac6dcfb6adc063aa570ab9aff37a93cab
Julia Kendler <jkendler@schueler.hakzell.at>
2020-03-27 09:23:33 +0100

    Dritte Aufgabe hinzugefügt

---
+++ jkendler_summe_3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jkendler_summe_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double zahl1;
+            double zahl2;
+
+            try
+            {
+
+                Console.Write("Geben Sie eine Zahl ein: ");
+                zahl1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Geben Sie eine Zahl ein: ");
+                zahl2 = Convert.ToDouble(Console.ReadLine());
+
+                if (zahl2 == 10)
+                {
+                    throw new Exception("10 ist nicht erlaubt");
+                }
+
+                double erg = zahl1 + zahl2;
+                Console.WriteLine(erg);
+
+
+
+              
+
+
+
+            }
+          
+            catch 
+            {
+                throw new Exception("10 ist nicht erlaubt");
+
+            }
+        }
+    }
+}
