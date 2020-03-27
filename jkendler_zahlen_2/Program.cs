a5dae71488baf6b5ed204e79e8dae5829d5309f2
Julia Kendler <jkendler@schueler.hakzell.at>
2020-03-27 09:06:31 +0100

    Zweite Aufgabe hinzugefügt

---
+++ jkendler_zahlen_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jkendler_zahlen_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Geben Sie eine Zahl ein: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Geben Sie eine Zahl ein: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+            }
+            catch (DivideByZeroException e)
+            {
+                throw new Exception("Man kann nicht durch null dividieren.");
+
+            }
+
+          
+       
+
+
+        }
+    }
+}
