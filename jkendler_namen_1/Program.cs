e56f85cd3d41d4844d25d8c52dff3c8e4490ac1f
Julia Kendler <jkendler@schueler.hakzell.at>
2020-03-27 09:05:09 +0100

    Erste Aufgabe hinzugefügt

---
+++ jkendler_namen_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jkendler_namen_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            try
+            {
+
+                string[] namen = new string[3];
+                string na;
+
+                for (int i = 0; i < 4 ; i++)
+                {
+                    
+                    Console.Write("Geben Sie einen Namen ein: ");
+                    na = Console.ReadLine();
+                    namen[i] = na;            
+
+                }
+
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw new Exception("Bitte nur drei Namen eingeben.");
+            }
+            
+
+
+        }
+    }
+}
